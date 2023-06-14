3dfe02a89210e9d12dab1c7221415a28b7d0887a
christina172 <117263200+christina172@users.noreply.github.com>
2023-06-14 18:15:07 +0300

    Rewrite the app using Express

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
-const http = require("http");
-const fs = require("fs");
-const path = require('path');
+const express = require('express');
+const app = express();
 
-const hostname = '127.0.0.1';
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-    let pathName;
-    res.statusCode = 200;
+app.use(express.static(__dirname));
 
-    let extension = path.extname(req.url);
-    if (extension == ".css") {
-        res.setHeader('Content-Type', 'text/css');
-        pathName = "styles.css";
-    } else if (extension == ".jpg") {
-        res.setHeader('Content-Type', 'image/jpg');
-        pathName = "woodland-sage.jpg";
-    } else {
-        res.setHeader('Content-Type', 'text/html');
-        if (req.url == "/") {
-            pathName = "index.html";
-        } else if (req.url == "/about") {
-            pathName = "about.html";
-        } else if (req.url == "/contact-me") {
-            pathName = "contact-me.html";
-        } else {
-            pathName = "404.html";
-            res.statusCode = 404;
-        };
-    }
-
-    fs.readFile(pathName, (err, data) => {
-        if (err) {
-            console.error(err);
-            res.end();
-        } else {
-            res.end(data);
-        }
-    });
+app.get("/", function (req, res) {
+    res.sendFile("./index.html", { root: __dirname });
 });
 
-server.listen(port);
+app.get("/about", function (req, res) {
+    res.sendFile("./about.html", { root: __dirname });
+});
+
+app.get("/contact-me", function (req, res) {
+    res.sendFile("./contact-me.html", { root: __dirname });
+});
+
+app.use(function (req, res) {
+    res.status(404).sendFile("./404.html", { root: __dirname });
+});
+
+app.listen(port);
